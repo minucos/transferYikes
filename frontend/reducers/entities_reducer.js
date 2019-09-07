47f6602f201033bf45244206d222b732c6ec77d3
andrew minucos <minucos@gmail.com>
2019-09-07 10:45:15 -0700

    Update entitiesReducer to remove currencies slice of state

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,13 +1,12 @@
 import { combineReducers } from 'redux';
 import UsersReducer from './users_reducer';
 import BalancesReducer from './wallets_reducer';
-import CurrenciesReducer from './currencies_reducer';
 import RatesReducer from './rates_reducer';
+// import CurrenciesReducer from './currencies_reducer';
 
 const EntitiesReducer = combineReducers({
     users: UsersReducer,
     balances: BalancesReducer,
-    currencies: CurrenciesReducer,
     rates: RatesReducer,
 })
 
